2c741821a5d23ee5973ab582ac3864f6237e4117
Tamara <29234416+tam92@users.noreply.github.com>
2023-06-11 12:17:58 +0200

    solved exercises

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+// farenheit to celsius
+const convertToCelsius = function(farenheit) {
+  return Math.round((farenheit - 32) * (5 / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+// celsius to farenheit
+const convertToFahrenheit = function(celsius) {
+  return Math.round((celsius * 9 / 5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
