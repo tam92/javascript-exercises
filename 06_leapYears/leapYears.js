6200c9aa2734c74ce5abdac667947a8d155f40d3
Tamara <29234416+tam92@users.noreply.github.com>
2023-06-11 11:52:22 +0200

    solved exercises

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // Leap years are years divisible by four (like 1984 and 2004).
 
+    if (year % 4 == 0) {
+        if ((year % 100 == 0) && (year % 400 !== 0)) {
+            return false;
+        }
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
