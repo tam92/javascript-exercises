499ba4badd0fecbe3b158d8fdb95e43fff38ab8c
Tamara <29234416+tam92@users.noreply.github.com>
2023-06-11 12:44:58 +0200

    solved exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(number1, number2) {
+	return number1 + number2;
 };
 
-const subtract = function() {
-	
+const subtract = function(number1, number2) {
+	return number1 - number2;
 };
 
-const sum = function() {
-	
+const sum = function(arrayOfNumbers) {
+	let totalSum = 0;
+  for (const iterator of arrayOfNumbers) {
+    totalSum += iterator;
+  }
+  return totalSum;
 };
 
-const multiply = function() {
-
+const multiply = function(arrayOfNumbers) {
+  let total;
+  for (const number of arrayOfNumbers) {
+    if (arrayOfNumbers[0] === number) {
+      total = number;
+    } else {
+      total *= number;
+    }
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(number1, number2) {
+	return number1 ** number2;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if (number == 0) return 1;
+  
+	return number * factorial(number-1);
 };
 
 // Do not edit below this line
