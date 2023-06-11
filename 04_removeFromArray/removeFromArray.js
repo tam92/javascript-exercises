19f030d20509d63c44c8b7525558a162da3a4cad
Tamara <29234416+tam92@users.noreply.github.com>
2023-06-11 11:12:52 +0200

    solved exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    
+    const newArray = [];
+    
+    array.forEach(element => {
+        // push all except if included in args
+        if (!args.includes(element)) {
+            newArray.push(element);
+        }
+    });
+    
+    return newArray;
 
+
+    // OPCION 2
+    // return array.filter(element => (!args.includes(element)));
 };
 
 // Do not edit below this line
