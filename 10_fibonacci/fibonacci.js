6b0dbe215afe2bd94d8fc2144284b38561518373
Tamara <29234416+tam92@users.noreply.github.com>
2023-06-11 14:44:32 +0200

    solved exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
 
+    if (position < 0) return "OOPS";
+    if (position === 0) return 0;
+
+    let a = 0;
+    let b = 1;
+
+    for (let index = 1; index < position; index++) {
+        const temp = b;
+        b = a + b;
+        a = temp;
+    }
+    return b;
 };
 
 // Do not edit below this line
