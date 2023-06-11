dae5a738ac04da5031840e10569683710e162a87
Tamara <29234416+tam92@users.noreply.github.com>
2023-06-11 14:09:38 +0200

    solved exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (palindrome) {
 
+    let test = palindrome.toLowerCase();
+    test = test.replace(/[^a-z]+/g, "");
+
+    // reverse the simple string
+    let reverse = test.split("").reverse().join("");
+
+    // return the comparison
+    return (test === reverse);
 };
 
 // Do not edit below this line
